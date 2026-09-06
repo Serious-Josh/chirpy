936899223f25521309262056168ca4816209910f
Serious-Josh <chapman.rjosh@gmail.com>
2026-09-06 18:18:49 +0000

    Added a bit more server code including endpoint handling testing. Moved example app files to new directory in src/app

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import express, { type Express } from 'express';
+import express, { Express, Request, response, Response } from 'express';
 
 export function main(){
     const app: Express = express();
     const port = 8080;
 
-    app.use(express.static("."));
+    app.use("/app", express.static("./src/app"));
+
+    app.get("/healthz", handlerReadiness);
 
     app.listen(port, () => {
         console.log(`Server running at http://localhost:${port}`);
     });
+}
+
+export function handlerReadiness(req: Request, res: Response){
+    res.set({
+        "Content-Type": "text/plain; charset=utf-8"
+    });
+
+    res.send(`OK`);
 }
 
 main();
