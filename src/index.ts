8769c1bafd56aa69045614e148f3ba10494c0d49
Serious-Josh <chapman.rjosh@gmail.com>
2026-09-06 05:24:33 +0000

    Added initial express server code, mainly for testing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import express, { type Express } from 'express';
+
 export function main(){
-    console.log("Hello, World!");
+    const app: Express = express();
+    const port = 8080;
+
+    app.use(express.static("."));
+
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    });
 }
 
 main();
