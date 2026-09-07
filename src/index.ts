284a0916f99f66cc0e74f077df56cc06517c087d
Serious-Josh <chapman.rjosh@gmail.com>
2026-09-07 13:13:35 +0000

    Added Config type with additional middleware handlers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express, { Express, Request, response, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import { config } from './config.js';
 
 export function main(){
     const app: Express = express();
     const port = 8080;
 
+    app.use("/app", middlewareMetricsInc);
     app.use("/app", express.static("./src/app"));
+    app.use(middlewareLogResponses);
 
     app.get("/healthz", handlerReadiness);
+    app.get("/metrics", handlerMetricsOutput);
+    app.get("/reset", handlerMetricsReset);
 
     app.listen(port, () => {
         console.log(`Server running at http://localhost:${port}`);
@@ -19,6 +24,34 @@ export function handlerReadiness(req: Request, res: Response){
     });
 
     res.send(`OK`);
+}
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction){
+    res.on("finish", () => {
+        const code = res.statusCode;
+        if(code != 200){
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${code}`);
+        }
+    });
+
+    next();
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction){
+    res.on("finish", () => {
+        config.fileserverHits += 1;
+    })
+
+    next();
+}
+
+function handlerMetricsOutput(req: Request, res: Response){
+    res.send(`Hits: ${config.fileserverHits}`);
+}
+
+function handlerMetricsReset(req: Request, res: Response){
+    config.fileserverHits = 0;
+    res.send("OK");
 }
 
 main();
